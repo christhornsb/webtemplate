281d8569ebe8243c61ae03a14c0bdfca1c9e22d8
christhornsb <90425848+christhornsb@users.noreply.github.com>
2022-04-03 19:26:51 -0500

    initial upload

---
+++ src/assets/images.js
@@ -0,0 +1,16 @@
+import logo from '../assets/logonew.png';
+import footage from '../assets/footage2.mp4';
+import logo2 from '../assets/logo2.png';
+
+
+
+
+
+
+
+
+export default {
+    logo,
+    footage,
+    logo2
+};
